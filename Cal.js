443619d59d508886327c17e52487ee64dc4439a7
felas <felafohim@gmail.com>
2024-07-05 00:01:59 -0400

    No comments

--- Cal.js
+++ Cal.js
@@ -35,4 +35,17 @@ console.log(operate(1,add,5))
 /// My alternative plan was to append an anonymous reduce function
 /// so that operation doesn't have to take any implicit arguments
 
+console.log(operate(5,multiply,5))
+HTMLDisplay = document.querySelector("#Display")
 
+function updateDisplay(number){
+        HTMLDisplay.textContent += number
+}
+btn1 = document.querySelector("#Numbers #one")
+btn5 = document.querySelector("#Numbers #five")
+btn5.addEventListener("Click", updateDisplay(5))
+btn1.addEventListener("click", updateDisplay(1))
+
+    numberedButtons = document.querySelector("#Numbers > div")
+    ///numberedButtons.forEach(button);{
+    ///button.addEventListener("click", updateDisplay
